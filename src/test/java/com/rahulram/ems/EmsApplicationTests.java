acada35791554ab4fc35e66e27ca3444d4524cc9
Rahulram240 <devendranrahulram@gmail.com>
2026-08-03 23:41:08 +0530

    Initial Commit

---
+++ src/test/java/com/rahulram/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulram.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
