9f2aac8fcaf4b513d4141efbdbcde5c7d302c342
Rahulram240 <dev.rahulram@gmail.com>
2026-08-04 13:49:09 +0530

    Added Department Entity

---
+++ src/main/java/com/rahulram/ems/entity/Department.java
@@ -0,0 +1,33 @@
+package com.rahulram.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "departments")
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String departmentName;
+
+	@Column(nullable = false)
+	private String departmentCode;
+
+	private String description;
+}
